27414f04476af5d70549702adfba239a0df3126f
Jeron Aldaron Lau <jeronlau@plopgrizzly.com>
2019-12-31 13:34:05 -0600

    Add `Format` trait.

--- src/lib.rs
+++ src/lib.rs
@@ -83,7 +83,7 @@ pub enum EncodeError {
     /// Couldn't parse file.
     ParseError(ParseError),
     /// Failed to read data.
-    WriteError,
+    WriteError(std::io::Error),
 }
 
 /// Metadata for raster.
@@ -169,12 +169,81 @@ where
 }
 
 use std::marker::PhantomData;
+use lodepng::ColorType;
+
+/// PNG compatible subset of pix `Format`s.
+pub trait Format: pix::Format {
+    /// Format to save as.
+    const PNG_COLOR: ColorType;
+    /// Bit Depth to save as.
+    const BIT_DEPTH: u32;
+}
+
+impl Format for pix::Gray8 {
+    const PNG_COLOR: ColorType = ColorType::Grey;
+    const BIT_DEPTH: u32 = 8;
+}
+
+impl Format for pix::Gray16 {
+    const PNG_COLOR: ColorType = ColorType::Grey;
+    const BIT_DEPTH: u32 = 16;
+}
+
+impl Format for pix::Gray32 {
+    const PNG_COLOR: ColorType = ColorType::Grey;
+    const BIT_DEPTH: u32 = 32;
+}
+
+impl Format for pix::GrayAlpha8 {
+    const PNG_COLOR: ColorType = ColorType::GreyAlpha;
+    const BIT_DEPTH: u32 = 8;
+}
+
+impl Format for pix::GrayAlpha16 {
+    const PNG_COLOR: ColorType = ColorType::GreyAlpha;
+    const BIT_DEPTH: u32 = 16;
+}
+
+impl Format for pix::GrayAlpha32 {
+    const PNG_COLOR: ColorType = ColorType::GreyAlpha;
+    const BIT_DEPTH: u32 = 32;
+}
+
+impl Format for pix::Rgb8 {
+    const PNG_COLOR: ColorType = ColorType::Rgb;
+    const BIT_DEPTH: u32 = 8;
+}
+
+impl Format for pix::Rgb16 {
+    const PNG_COLOR: ColorType = ColorType::Rgb;
+    const BIT_DEPTH: u32 = 16;
+}
+
+impl Format for pix::Rgb32 {
+    const PNG_COLOR: ColorType = ColorType::Rgb;
+    const BIT_DEPTH: u32 = 32;
+}
+
+impl Format for pix::Rgba8 {
+    const PNG_COLOR: ColorType = ColorType::Rgba;
+    const BIT_DEPTH: u32 = 8;
+}
+
+impl Format for pix::Rgba16 {
+    const PNG_COLOR: ColorType = ColorType::Rgba;
+    const BIT_DEPTH: u32 = 16;
+}
+
+impl Format for pix::Rgba32 {
+    const PNG_COLOR: ColorType = ColorType::Rgba;
+    const BIT_DEPTH: u32 = 32;
+}
 
 /// Encoder for writing [`Raster`](chunk/enum.Raster.html)s into a PNG file.
 pub struct RasterEncoder<'a, W, F>
 where
     W: std::io::Write,
-    F: pix::Format,
+    F: Format,
 {
     state: &'a mut lodepng::State,
     bytes: W,
@@ -184,7 +253,7 @@ where
 impl<'a, W, F> RasterEncoder<'a, W, F>
 where
     W: std::io::Write,
-    F: pix::Format,
+    F: Format,
 {
     /// Add a frame to the animation or still.
     pub fn add_frame(
@@ -201,7 +270,7 @@ where
 
         match self.bytes.write(&bytes) {
             Ok(_size) => Ok(()),
-            Err(_) => return Err(EncodeError::WriteError),
+            Err(e) => return Err(EncodeError::WriteError(e)),
         }
     }
 }
@@ -284,7 +353,7 @@ impl EncoderBuilder {
     pub fn encode_rasters<'a, W, F>(&'a mut self, bytes: W) -> RasterEncoder<'a, W, F>
     where
         W: std::io::Write,
-        F: pix::Format,
+        F: Format,
     {
         RasterEncoder {
             state: &mut self.state,
